564117484cdccbc617168b073b6a70000275c92a
Anubhav Narayan Singh <122772894+Anubhav-GEU@users.noreply.github.com>
2024-01-01 17:14:04 +0530

    Create 8_findConsecutiveIntegerFromADataStream.cpp

---
+++ Goldman Sachs/8_findConsecutiveIntegerFromADataStream.cpp
@@ -0,0 +1,21 @@
+class DataStream {
+public:
+    long long val, main, count;
+    DataStream(int value, int k) {
+        val = value;
+        main = k;
+        count = 0;
+    }
+    
+    bool consec(int num) {
+        if (num == val) count++;
+        else count = 0;
+        return count >= main;
+    }
+};
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream* obj = new DataStream(value, k);
+ * bool param_1 = obj->consec(num);
+ */
